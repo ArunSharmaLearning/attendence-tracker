b57a44d2f369b940b5ba826b403bd076ba2d4a91
ArunSharmaLearning <arunlivingfantastico@gmail.com>
2022-09-28 01:52:38 +0530

    seperate folder for database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const studentRouter =require('./router/student')
 const staffRouter =require('./router/staff')
 
-require('./models/db');
+require('./database/db');
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
